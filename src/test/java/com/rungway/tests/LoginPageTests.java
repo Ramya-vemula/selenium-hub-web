eab8adae6c9dbd0e18ff6225ae618eec2bdec2f7
Rohith Vitta <rohith.vitta@rungway.com>
2018-02-13 15:31:09 +0000

    Removed description tags from tests and tests should pass now.

--- src/test/java/com/rungway/tests/LoginPageTests.java
+++ src/test/java/com/rungway/tests/LoginPageTests.java
@@ -9,15 +9,13 @@ import org.testng.annotations.Test;
 public class LoginPageTests extends BaseTests {
 
   @Test
-  @Description("This test will fail, due to assertion")
   public void firstTest() {
   	driver.get("https://rungway-test.dev.rungway.uk/");
     LoginPage loginPage = new LoginPage(driver);
-    Assert.assertEquals(loginPage.pageTitleText(), "Log n");
+    Assert.assertEquals(loginPage.pageTitleText(), "Log in");
   }
 
   @Test
-  @Description("This test should pass")
   public void secondTest() {
 
 	  driver.get("https://rungway-test.dev.rungway.uk/");
