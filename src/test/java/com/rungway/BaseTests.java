09e244cc4a4e074b8e06d1376479408e5f722ed0
Rohith Vitta <rohith.vitta@rungway.com>
2018-04-17 15:14:30 +0100

    Added path to geko and chrome driver to properties file

--- src/test/java/com/rungway/BaseTests.java
+++ src/test/java/com/rungway/BaseTests.java
@@ -3,12 +3,12 @@ package com.rungway;
 
 import com.rungway.utils.TestData;
 import io.qameta.allure.Attachment;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,26 +19,27 @@ public abstract class BaseTests {
 
     public WebDriver driver;
 
+    public WebDriverWait wait;
+
     @BeforeMethod(alwaysRun = true)
     @Parameters("browser")
     public void setup(final String browser) throws Exception {
         if (browser.equalsIgnoreCase("firefox")) {
 
-            //path to geckodriver
-            System.setProperty("webdriver.gecko.driver", "/Users/Rohith/selenium/geckodriver");
+            System.setProperty("webdriver.gecko.driver", TestData.getProperty("GEKO_DRIVER_PATH"));
             driver = new FirefoxDriver();
         } else if (browser.equalsIgnoreCase("chrome")) {
 
-            //path to chromedriver
-            System.setProperty("webdriver.chrome.driver", "/Users/Rohith/selenium/chromedriver");
+            System.setProperty("webdriver.chrome.driver", TestData.getProperty("CHROME_DRIVER_PATH"));
             driver = new ChromeDriver();
         } else if (browser.equalsIgnoreCase("safari")) {
+
             driver = new SafariDriver();
         } else {
             //If no browser passed throw exception
             throw new Exception("Browser is not correct");
         }
-
+        wait = new WebDriverWait(driver, 15);
         driver.manage().window().maximize();
         driver.get(TestData.getProperty("BASE_URL"));
 
@@ -56,5 +57,25 @@ public abstract class BaseTests {
     @Attachment(value = "Page Screenshot", type = "image/png")
     private byte[] saveScreenshot(final WebDriver driver) {
         return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    public void waitForVisibilityOfElement(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+
+    }
+
+    public void waitForVisibilityOfElement(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+    }
+
+    public void waitForElementToDisappear(WebElement element) {
+        wait.until(ExpectedConditions.invisibilityOf(element));
+
+    }
+
+    public void waitForElementToDisappear(By locator) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+
     }
 }
