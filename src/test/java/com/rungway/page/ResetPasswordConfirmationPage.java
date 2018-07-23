8f7965dd6f00048caf20cb2bb500d900728d19d2
Rohith Vitta <rohith.vitta@rungway.com>
2018-07-23 13:42:21 +0100

    Added reset password and confirmation pages

---
+++ src/test/java/com/rungway/page/ResetPasswordConfirmationPage.java
@@ -0,0 +1,27 @@
+package com.rungway.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class ResetPasswordConfirmationPage {
+
+    @FindBy(id = "page_header")
+    public WebElement pageTitle;
+
+    @FindBy(css = "(.uk-margin-top > a")
+    public WebElement didntRecieveEmailLink;
+
+    @FindBy(className = "message")
+    public WebElement messageArea;
+
+    private WebDriver driver;
+
+    public ResetPasswordConfirmationPage(final WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 15), this);
+    }
+}
